87eeb8475216f31bbde96137361024bfee417914
eduard <burak.edouard@yandex.by>
2023-01-02 15:01:32 +0300

    add entries

--- django_site1/settings.py
+++ django_site1/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'entries.apps.EntriesConfig',
+    'riddles.apps.RiddlesConfig'
 ]
 
 MIDDLEWARE = [
